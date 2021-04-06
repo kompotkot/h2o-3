d2d5a81c55b5fa8cd82fa968637a5f9567f8e292
kompotkot <zenitsws@gmail.com>
2021-04-06 13:03:27 +0000

    Vversion file and updated reporter for notebook

--- h2o-py/h2o/utils/reporter.py
+++ h2o-py/h2o/utils/reporter.py
@@ -1,5 +1,6 @@
-import sys
 import uuid
+
+from .version import __version__
 
 from humbug.consent import HumbugConsent
 from humbug.report import Reporter, Modes
@@ -11,7 +12,8 @@ session_id = str(uuid.uuid4())
 session_id_tag = "session_id:{}".format(session_id)
 
 
-h2o_tags = ["123"]
+h2o_tags = [__version__]
+
 
 class ConsentState:
     def __init__(self, consent: bool = False) -> None:
@@ -19,6 +21,7 @@ class ConsentState:
 
     def check(self) -> bool:
         return self.consent
+
 
 h2o_consent_state = ConsentState(True)
 
@@ -30,19 +33,6 @@ h2o_reporter = Reporter(
     session_id=session_id,
     bugout_token=HUMBUG_TOKEN,
     bugout_journal_id=HUMBUG_KB_ID,
-
+    mode=Modes.DEFAULT,
 )
-
-def setup_excepthook():
-    h2o_reporter.system_report(publish=True, tags=h2o_tags)
-    try:
-        ipython_shell = get_ipython()
-        old_showtraceback = ipython_shell.showtraceback
-        def showtraceback(*args, **kwargs):
-            _, exc_instance, _ = sys.exc_info()
-            h2o_reporter.error_report(exc_instance, tags=h2o_tags, publish=True)
-            old_showtraceback(*args, **kwargs)
-
-        ipython_shell.showtraceback = showtraceback
-    except NameError:
-        h2o_reporter.setup_excepthook(publish=True, tags=h2o_tags)
+notebook_reporter = h2o_reporter.setup_notebook_excepthook(tags=h2o_tags)
